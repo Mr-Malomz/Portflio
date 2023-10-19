33512021aca5635f774c498de9717ba26b57ef59
demola malomo <malomo.alaba@yahoo.com>
2023-10-19 13:46:20 +0100

    upgraded version

--- src/components/HomeSVG.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HomeSVG = (props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>) => (
-	<svg  viewBox="0 0 723 720" fill="none" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" preserveAspectRatio="none" >
-        <path d="M0 0H723L547 720H0V0Z" fill="#FFFEFC"/>
-    </svg>
-);
-
-export default HomeSVG;
